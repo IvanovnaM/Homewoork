db8e08e1fab3eb2c5d62aaaef64f7aa474fc4d37
IvanovnaM <lungrenok69@gmail.com>
2024-01-25 21:55:41 +0300

    4 commit

--- Aston/src/main/java/shcool/aston/Employee.java
+++ Aston/src/main/java/shcool/aston/Employee.java
@@ -4,13 +4,13 @@ package shcool.aston;
 Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль. */
 
 public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String number;
+    private int salary;
+    private int age;
 
-    String name;
-    String position;
-    String email;
-    String number;
-    int salary;
-    int age;
 
     public Employee(String name, String position, String email, String number, int salary, int age) {
         this.name = name;
@@ -19,31 +19,28 @@ public class Employee {
         this.number = number;
         this.salary = salary;
         this.age = age;
+
+    }
+
+    @Override
+    public String toString() {
+        return (name + "\n-"
+                + position + "\n-"
+                + email + "\n-"
+                + number + "\n-"
+                + salary + "\n-"
+                + age);
     }
 
     public void printEmployeeInfo() {
-        System.out.println("ФИО : " + this.name);
-        System.out.println("Должность : " + this.position);
-        System.out.println("Email : " + this.email);
-        System.out.println("Номер телефона : " + this.number);
-        System.out.println("Зарплата : " + this.salary);
-        System.out.println("Возраст : " + this.age);
+        System.out.println("ФИО : " + name);
+        System.out.println("Должность : " + position);
+        System.out.println("Email : " + email);
+        System.out.println("Номер телефона : " + number);
+        System.out.println("Зарплата : " + salary);
+        System.out.println("Возраст : " + age);
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        Employee[] employeesArray = new Employee[5];
-        employeesArray[0] = new Employee("Chad Turner", "Engineer",
-                "Chad Turner@gmail.com", "11111111111", 1000, 30);
-        employeesArray[1] = new Employee("Alma Thomas", "Job title 2",
-                "Alma Thomas@gmail.com", "2222222222", 1100, 20);
-        employeesArray[2] = new Employee("Catherine Adkins", "Job title 3",
-                "Catherine Adkins@gmail.com", "3333333333", 1200, 25);
-        employeesArray[3] = new Employee("Nellie Brown", "Job title 4",
-                "Nellie Brown@gmail.com", "44444444", 1300, 37);
-        employeesArray[4] = new Employee("Mary Lopez", "Job title 5",
-                "Mary Lopez@gmail.com", "555555555", 1400, 23);
-
-    }
 
 }
--- Aston/src/main/java/shcool/aston/Park.java
+++ Aston/src/main/java/shcool/aston/Park.java
@@ -3,6 +3,13 @@ package shcool.aston;
         об аттракционах, времени их работы и стоимости.*/
 
 public class Park {
+    private Attraction attr1;
+    private Attraction attr2;
+    private Attraction attr3;
+    private Attraction attr4;
+    private Attraction attr5;
+
+
     public class Attraction {
         private String attrName;
         private String workingHours;
@@ -13,15 +20,40 @@ public class Park {
                     + "Working Hours : " + workingHours + "\n"
                     + "Price : " + price);
         }
+
     }
 
     public static void main(String[] args) {
         Park park = new Park();
-        Park.Attraction attr1 = park.new Attraction();
+        Attraction attr1 = park.new Attraction();
+        Attraction attr2 = park.new Attraction();
+        Attraction attr3 = park.new Attraction();
+        Attraction attr4 = park.new Attraction();
+        Attraction attr5 = park.new Attraction();
 
         attr1.attrName = "Ferris wheel";
         attr1.workingHours = "9:00 - 17:00";
         attr1.price = 42;
         attr1.getAttrInfo();
+
+        attr2.attrName = "Carousel";
+        attr2.workingHours = "9:00 - 17:00";
+        attr2.price = 5;
+        attr2.getAttrInfo();
+
+        attr3.attrName = "Roller coaster";
+        attr3.workingHours = "9:00 - 17:00";
+        attr3.price = 16;
+        attr3.getAttrInfo();
+
+        attr4.attrName = "Pirate ship rides";
+        attr4.workingHours = "9:00 - 17:00";
+        attr4.price = 18;
+        attr4.getAttrInfo();
+
+        attr5.attrName = "Train rides";
+        attr5.workingHours = "9:00 - 17:00";
+        attr5.price = 12;
+        attr5.getAttrInfo();
     }
 }
