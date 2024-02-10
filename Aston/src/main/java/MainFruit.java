0a54bd88e4ab3abf2230868fe40bdf604ae21632
IvanovnaM <lungrenok69@gmail.com>
2024-02-10 21:54:48 +0300

    10 commit

--- Aston/src/main/java/MainFruit.java
+++ Aston/src/main/java/MainFruit.java
@@ -3,15 +3,15 @@ import java.util.ArrayList;
 public class MainFruit {
     public static void main(String[] args) {
         Box<Apple> appleBox = new Box<>(new ArrayList<>());
-        appleBox.addFruitsToBox(new Apple());
-        appleBox.addFruitsToBox(new Apple());
-        appleBox.addFruitsToBox(new Apple());
-        appleBox.addFruitsToBox(new Apple());
+        for (int i = 0; i < 15; i++) {
+            appleBox.addFruitsToBox(new Apple());
+        }
         System.out.println("Вес Apple в  коробке= " + appleBox.getWeight());               // Получаем вес коробки с яблоками
 
         Box<Orange> orangeBox = new Box<>(new ArrayList<>());
-        orangeBox.addFruitsToBox(new Orange());
-        orangeBox.addFruitsToBox(new Orange());
+        for (int i = 0; i < 4; i++) {
+            orangeBox.addFruitsToBox(new Orange());
+        }
         System.out.println("Вес Orange в  коробке= " + orangeBox.getWeight());              // Получаем вес коробки с апельсинами
 
         System.out.println(appleBox.compare(orangeBox));        // Сравниваем две коробки с фруктами
@@ -19,6 +19,5 @@ public class MainFruit {
         Box<Apple> appleBox2 = new Box<>(new ArrayList<>());
         appleBox.putFruitInAnotherBox(appleBox2);               // Перекидываем фрукты из одной коробки в другую
         System.out.println(appleBox2);
-        System.out.println(appleBox);
     }
 }
